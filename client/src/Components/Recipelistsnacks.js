b4dd3d0329694d3b13bc7a8e062d9d7797bc8741
Harshitha-Lanka <125571354+Harshitha-Lanka@users.noreply.github.com>
2025-06-29 13:08:50 +0530

    Update Recipelistsnacks.js

--- client/src/Components/Recipelistsnacks.js
+++ client/src/Components/Recipelistsnacks.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // Import Link for navigation
 import styles from './Component1.module.css';
+import axios from "axios";
 
 const RecipeListSnacks = () => {
     const [recipes, setRecipes] = useState([]);
