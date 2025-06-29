4938482cd66849d249da6ecd401cec443c7bac71
Harshitha-Lanka <125571354+Harshitha-Lanka@users.noreply.github.com>
2025-06-29 12:26:52 +0530

    Update RecipeListBreakfast.js

--- client/src/Components/RecipeListBreakfast.js
+++ client/src/Components/RecipeListBreakfast.js
@@ -9,7 +9,8 @@ const BreakfastRecipeList = () => {
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await fetch("http://localhost:5000/recipes");
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/recipes`);
+
         const data = await response.json();
 
         // Filter only Breakfast recipes
