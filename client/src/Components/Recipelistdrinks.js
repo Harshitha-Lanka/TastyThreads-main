4238e647d9723469fbd48133c1a7a7b991d788a9
Harshitha-Lanka <125571354+Harshitha-Lanka@users.noreply.github.com>
2025-06-29 13:03:31 +0530

    Update Recipelistdrinks.js

--- client/src/Components/Recipelistdrinks.js
+++ client/src/Components/Recipelistdrinks.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // Import Link for navigation
 import styles from './Component1.module.css';
+import axios from "axios";
 
 const RecipeListDrinks = () => {
     const [recipes, setRecipes] = useState([]);
@@ -9,7 +10,8 @@ const RecipeListDrinks = () => {
     useEffect(() => {
         const fetchRecipes = async () => {
             try {
-                const response = await fetch("http://localhost:5000/recipes");
+                const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/recipes`);
+
                 const data = await response.json();
 
                 // Filter only Drinks recipes
